3321d4371dfba3a5a75e2f820519de8b56a4a5b2
Javiera <pilquimanjaviera@gmail.com>
2023-09-19 16:29:47 -0400

    Vista app1

--- Proyecto/urls.py
+++ Proyecto/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app1 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hola/', views.display),
+    path('ahora/', views.displayDateTime),
 ]
